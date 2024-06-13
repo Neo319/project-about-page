3816acd6a9dbfeb291e05775d9ca9eb764116ad0
CastorDisaster <alexmnevins@gmail.com>
2024-06-13 11:00:33 -0400

    implement try/catch error handling

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
 
-    if (req.url === "/" && req.method === "GET") {
-      res.end("Homepage success");
-    } else {
-      console.log("Root: " + req.url);
-      throw new Error("incorrect root");
+    try {
+      if (req.url === "/" && req.method === "GET") {
+        res.end("Homepage success");
+      } else {
+        console.log("Root: " + req.url);
+        throw new Error("incorrect root");
+      }
+    } catch (error) {
+      console.error(error.message);
+      res.writeHead(500, { "Content-type": "text/plain" });
+      res.end("Internal Server Error");
     }
   })
-  .listen(8080);
+  .listen(8080, () => {
+    console.log("Server listening on port 8080");
+  });
