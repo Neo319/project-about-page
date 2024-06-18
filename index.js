fe63927fb1ae2515bed4a4718875fdac8183f8ab
CastorDisaster <alexmnevins@gmail.com>
2024-06-18 15:03:40 -0400

    implement simple error handling

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // ------------------------------------- EXPRESS VERSION
 
 import express from "express";
-import path from "path";
 
 const app = express();
 const port = 8080;
 
 // Middleware to serve static files from the 'public' directory
 app.use(express.static("public"));
+
+// Middleware to handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, we couldn't find that!");
+});
+
+// Middleware to handle other errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
