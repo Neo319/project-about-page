9600a0cdf270cfe51811bfc6a34cc1d760ff6b3f
CastorDisaster <alexmnevins@gmail.com>
2024-06-18 14:26:32 -0400

    create base for express-powered server

--- index.js
+++ index.js
@@ -1,58 +1,81 @@
-import http from "http";
-import fs from "node:fs";
+// ------------------------------------- EXPRESS VERSION
 
-console.log("hello world");
+import express from "express";
+const app = express();
+const port = 8080;
 
-http
-  .createServer((req, res) => {
-    try {
-      if (req.url === "/" && req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
+app.get("/", function (req, res) {
+  res.send("Hello World~!");
+});
 
-        try {
-          const data = fs.readFileSync("./public/index.html", "utf8");
-          res.write(data);
-        } catch (err) {
-          console.error(err);
-        }
-        res.end();
-      } else if (req.url === "/about" && req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
+app.listen(port, function () {
+  console.log(`Example app listening on port 
+    ${port}!`);
+});
 
-        try {
-          const data = fs.readFileSync("./public/about.html", "utf8");
-          res.write(data);
-        } catch (err) {
-          console.error(err);
-        }
-        res.end();
-      } else if (req.url === "/contact-me" && req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
+//
 
-        try {
-          const data = fs.readFileSync("./public/contact-me.html", "utf8");
-          res.write(data);
-        } catch (err) {
-          console.error(err);
-        }
-        res.end();
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
+//
 
-        try {
-          const data = fs.readFileSync("./public/404.html", "utf8");
-          res.write(data);
-        } catch (err) {
-          console.error(err);
-        }
-        res.end();
-      }
-    } catch (error) {
-      console.error(error.message);
-      res.writeHead(500, { "Content-type": "text/plain" });
-      res.end("Internal Server Error");
-    }
-  })
-  .listen(8080, () => {
-    console.log("Server listening on port 8080");
-  });
+//
+
+// ------------------------------------- HTTP / STANDARD VERSION
+
+// import http from "http";
+// import fs from "node:fs";
+
+// console.log("hello world");
+
+// http
+//   .createServer((req, res) => {
+//     try {
+//       if (req.url === "/" && req.method === "GET") {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+
+//         try {
+//           const data = fs.readFileSync("./public/index.html", "utf8");
+//           res.write(data);
+//         } catch (err) {
+//           console.error(err);
+//         }
+//         res.end();
+//       } else if (req.url === "/about" && req.method === "GET") {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+
+//         try {
+//           const data = fs.readFileSync("./public/about.html", "utf8");
+//           res.write(data);
+//         } catch (err) {
+//           console.error(err);
+//         }
+//         res.end();
+//       } else if (req.url === "/contact-me" && req.method === "GET") {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+
+//         try {
+//           const data = fs.readFileSync("./public/contact-me.html", "utf8");
+//           res.write(data);
+//         } catch (err) {
+//           console.error(err);
+//         }
+//         res.end();
+//       } else {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+
+//         try {
+//           const data = fs.readFileSync("./public/404.html", "utf8");
+//           res.write(data);
+//         } catch (err) {
+//           console.error(err);
+//         }
+//         res.end();
+//       }
+//     } catch (error) {
+//       console.error(error.message);
+//       res.writeHead(500, { "Content-type": "text/plain" });
+//       res.end("Internal Server Error");
+//     }
+//   })
+//   .listen(8080, () => {
+//     console.log("Server listening on port 8080");
+//   });
